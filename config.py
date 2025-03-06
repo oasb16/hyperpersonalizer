ba5979acc255b84e09bb1ff3e57857b5c3ec2b73
Omkar Sawant Bhosle <omkarsawantbhosle@Omkars-MacBook-Pro.local>
2025-03-06 10:22:28 -0800

    Initial commit

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 COGNITO_DOMAIN = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_l2tI8cmwZ"
 COGNITO_CLIENT_ID = "71mr4dkg933tqhsnokqd9nk13n"
 COGNITO_CLIENT_SECRET = os.getenv("COGNITO_CLIENT_SECRET")  # Keep secret in env variables
-COGNITO_REDIRECT_URI = "https://hyperpersonalizer-a0808b1ef9ed.herokuapp.com//authorize"
+COGNITO_REDIRECT_URI = "https://hyperpersonalizer-a0808b1ef9ed.herokuapp.com/authorize"
 
 # AWS Cognito OpenID Configuration
 COGNITO_METADATA_URL = f"{COGNITO_DOMAIN}/.well-known/openid-configuration"
